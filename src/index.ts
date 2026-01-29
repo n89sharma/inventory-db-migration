f556f59e07b1ad2ffea6641e46fa81f61abc06cc
Nikhil Sharma <n89sharma@gmail.com>
2026-01-29 15:36:20 -0500

    MySQL connection added

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 import { prisma } from './prisma.js'
+import mysql, { RowDataPacket } from 'mysql2/promise';
 
-const message: string = '1Hello, Node.js with TypeScript!';
-console.log(message);
+const con = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    port: parseInt(process.env.DB_PORT!)
+})
 
 async function main() {
-    const allModels = await prisma.model.findMany({
-        include: {
-            brand: true
-        },
-    })
-    console.log('All users:', JSON.stringify(allModels, null, 2))
+    const brands = await prisma.brand.findFirst()
+    console.log(brands)
+    return 0
 }
 
 main()
     .then(async () => {
         await prisma.$disconnect()
+        await con.end()
+        process.exit(0)
     })
     .catch(async (e) => {
         console.error(e)
