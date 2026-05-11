bf03e8a94b306962298b0b0fecc86da2db2556ed
Nikhil Sharma <n89sharma@gmail.com>
2026-05-11 10:34:55 -0400

    entity id generated through pgsql sequences

---
+++ prisma/functions/createSequence.sql
@@ -0,0 +1,6 @@
+  CREATE SEQUENCE seq_asset     START 1;
+  CREATE SEQUENCE seq_arrival   START 1;
+  CREATE SEQUENCE seq_departure START 1;
+  CREATE SEQUENCE seq_transfer  START 1;
+  CREATE SEQUENCE seq_hold      START 1;
+  CREATE SEQUENCE seq_invoice   START 1;
